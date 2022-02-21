7f556da2c865fed3d4e041046ae718a601ab6cb9
Ben Li-Sauerwine <bsauerwine@gmail.com>
2022-02-21 00:48:07 -0500

    Delete unneeded custom_pigen stuff from the /simpleaq/ directory.

--- custom_pigen/stage2/04-install-requirements/03-run.sh
+++ custom_pigen/stage2/04-install-requirements/03-run.sh
@@ -2,6 +2,14 @@
 
 cp -R /simpleaq "${ROOTFS_DIR}"
 
+# Install SimpleAQ requirements.
 on_chroot << EOF
         pip install -r /simpleaq/requirements.txt
 EOF
+
+# Delete unnecessary custom pigen stuff.
+on_chroot << EOF
+        rm -rf /simpleaq/custom_pigen
+EOF
+
+
