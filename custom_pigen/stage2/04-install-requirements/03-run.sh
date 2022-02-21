09c0e68a60d93a52751cee1a9a86ab6def0f15ee
Ben Li-Sauerwine <bsauerwine@gmail.com>
2022-02-21 02:56:06 -0500

    Attempt to start the SimpleAQ recorder as a systemd service.

--- custom_pigen/stage2/04-install-requirements/03-run.sh
+++ custom_pigen/stage2/04-install-requirements/03-run.sh
@@ -7,9 +7,27 @@ on_chroot << EOF
         pip install -r /simpleaq/requirements.txt
 EOF
 
-# Delete unnecessary custom pigen stuff.
+# Set up a system-scoped systemd service.
+# This is actually necessary because user-scoped services will not actually
+# run until the user first logs in, and by default will terminate when the
+# user logs out.  We obviously cannot rely on FIRST_USER_NAME ever logging
+# in for most users.
+# https://github.com/torfsen/python-systemd-tutorial is awesome.
+cp files/simpleaq.service "${ROOTFS_DIR}/etc/systemd/system"
+
+# SimpleAQ uses python-dotenv.
+# We will set the environment variables for SimpleAQ at the system level.
+cat /simpleaq/example.env >> "${ROOTFS_DIR}/etc/environment"
+
+# Make sure our service has the right permissions and that it starts on boot.
+on_chroot << EOF
+        chown root:root /etc/systemd/system/simpleaq.service
+        chmod 644 /etc/systemd/system/simpleaq.service
+        systemctl enable simpleaq
+EOF
+
+# Delete now-unnecessary custom pigen stuff.
 on_chroot << EOF
         rm -rf /simpleaq/custom_pigen
 EOF
-
 
