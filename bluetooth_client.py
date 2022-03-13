00eb611a84d06ae41cc8627fff8f38a99736d4ca
Ben Li-Sauerwine <bsauerwine@gmail.com>
2022-03-13 04:02:03 -0400

    A much more robust bluetooth client, which will sit there and scan for the desired device until it's found.

--- bluetooth_client.py
+++ bluetooth_client.py
@@ -8,47 +8,76 @@ import logging
 import sys
 
 
-async def find_simpleaq_device():
-  try:
-    nearby_devices = await discover() 
+async def find_simpleaq_device(prefix):
+  nearby_devices = await discover() 
 
-    for device in nearby_devices:
-      print("Device:")
-      print(device)
-      try:
-        async with BleakClient(device, timeout=30) as client:
-          svcs = await client.get_services()
-          print("Services:")
-          for service in svcs:
-            print(service)
-      except Exception as scan_err:
-        logging.error(str(scan_err))
+  for device in nearby_devices:
+    logging.debug("Found device while scanning: '{}'".format(device))
+    if device.name.lower().startswith(prefix.lower()):
+      return device
 
-  except Exception as err:
-    logging.info("Are you sure the device you're using supports bluetooth?")
-    logging.info("If you're sure you have working bluetooth on Ubuntu, you could try: ")
-    logging.info("sudo rfkill unblock bluetooth")
-    logging.info("sudo systemctl restart bluetooth")
-    logging.info("sudo systemctl status bluetooth")
-    logging.error(str(err))
-    return 
+  # For some reason, the device name sometimes isn't recognized
+  # until we connect at least once.
+  for device in nearby_devices:
+    try:
+      if device.name.lower().startswith(prefix.lower()):
+        return device
+
+      # For some reason, the device name sometimes isn't recognized
+      # until we connect at least once.
+      async with BleakClient(device, timeout=10) as client:
+        logging.debug("Connected to device: '{}'".format(device))
+#        svcs = await client.get_services()
+    except Exception as err:
+      logging.debug("Failed to connect to device {}: {}".format(device, str(err)))
 
 
 async def main(args):
   # Configure logging.
-  logging.basicConfig(stream=sys.stderr, level=logging.INFO)
+  loglevels = {
+      'critical': logging.CRITICAL,
+      'error': logging.ERROR,
+      'warning': logging.WARNING,
+      'info': logging.INFO,
+      'debug': logging.DEBUG
+  }
 
-  # Find the SimpleAQ device
-  try:
-    device = await find_simpleaq_device()
-  except Exception as err:
-    logging.info("Are you sure the device you're using supports bluetooth?")
-    logging.error(str(err))
-    return 1 
+  logging.basicConfig(stream=sys.stderr, level=loglevels[args.loglevel])
+
+  # Let's search for the device.
+  logging.info("Scanning for device prefixed with {}.  Press Ctrl+C to cancel.".format(args.name))
+  device = None
+  while not device:
+    try:
+      device = await find_simpleaq_device(args.name)
+    except Exception as err:
+      logging.info("Are you sure the device you're using supports bluetooth?")
+      logging.info("If you're sure you have working bluetooth on Ubuntu, you could try: ")
+      logging.info("sudo rfkill unblock bluetooth")
+      logging.info("sudo systemctl restart bluetooth")
+      logging.info("sudo systemctl status bluetooth")
+      logging.error(str(err))
+      return
+
+  logging.info("Found SimpleAQ device: {}".format(device))
+
+  # Ok, we found the device.
+
+#      try:
+#        async with BleakClient(device, timeout=30) as client:
+#          svcs = await client.get_services()
+#          print("Services:")
+#          for service in svcs:
+#            print(service)
+#      except Exception as scan_err:
+#        logging.error(str(scan_err))
 
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser(description="Bluetooth listener for SimpleAQ device.")
+  parser = argparse.ArgumentParser(description="Bluetooth programmer for SimpleAQ device.")
+
+  parser.add_argument('--name', help="The case-insensitive device name, or a prefix thereof, to look for.", default='simpleaq')
+  parser.add_argument('--loglevel', help="Log level for device.", default='info', choices=['info', 'debug', 'warn', 'error', 'critical'])
 
   args = parser.parse_args()
 
