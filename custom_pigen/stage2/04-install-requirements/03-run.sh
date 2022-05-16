ff41a01df2d329a3e1e856d5d0805f73a68a3443
Ben Li-Sauerwine <bsauerwine@gmail.com>
2022-05-16 04:55:39 -0400

    Beginnings of an easier wlan0-hostap switching story following:  https://raspberrypi.stackexchange.com/questions/93311/switch-between-wifi-client-and-access-point-without-reboot

--- custom_pigen/stage2/04-install-requirements/03-run.sh
+++ custom_pigen/stage2/04-install-requirements/03-run.sh
@@ -46,7 +46,43 @@ on_chroot << EOF
         systemctl disable hostapd.service
 EOF
 
-# TODO:  I'm not sure this is actually necessary.
-# cat files/dnsmasq-extra.conf >> "${ROOTFS_DIR}/etc/dnsmasq.conf"
-cp files/hostapd.conf "${ROOTFS_DIR}/etc/hostapd/hostapd.conf"
-cp files/rc.local "${ROOTFS_DIR}/etc/rc.local"
+# Following instructions at:
+# https://raspberrypi.stackexchange.com/questions/93311/switch-between-wifi-client-and-access-point-without-reboot
+
+# Disable Debian networking and dhcpcd
+on_chroot << EOF
+        systemctl mask networking.service dhcpcd.service
+        mv /etc/network/interfaces /etc/network/interfaces-
+        sed -i '1i resolvconf=NO' /etc/resolvconf.conf
+EOF
+
+# Enable systemd-networkd
+on_chroot << EOF
+        systemctl enable systemd-networkd.service
+        systemctl enable systemd-resolved.conf
+        ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
+EOF
+
+# The wifi client must be on wlan0.
+on_chroot << EOF
+        mv /etc/wpa_supplicant/wpa_supplicant.conf /etc/wpa_supplicant/wpa_supplicant-wlan0.conf
+        systemctl disable wpa_supplicant.service
+        systemctl enable wpa_supplicant@wlan0.service
+EOF
+
+cp files/08-wlan0.network "${ROOTFS_DIR}/etc/systemd/network"
+
+# The hostap is on ap0.
+cp files/wpa_supplicant-ap0.conf "${ROOTFS_DIR}/etc/wpa_supplicant/wpa_supplicant-ap0.conf"
+cp files/12-ap0.network   "${ROOTFS_DIR}/etc/systemd/network"
+
+# The hostap starts "off".  We'll switch it on if needed.
+on_chroot << EOF
+        systemctl disable wpa_supplicant@ap0.service
+EOF
+
+# TODO:  Need to figure out how to set up wpa_supplicant@ap0.service appropriately.
+#        This will be easier to do once we've got an otherwise valid file to look at and play with.
+
+# TODO:  Re-add this using the wpa_supplicant hostap instead of hostapd
+# cp files/rc.local "${ROOTFS_DIR}/etc/rc.local"
