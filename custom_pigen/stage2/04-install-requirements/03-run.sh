d9698b220c7cb8e21b0c72b8cf4f085fe63fd7c9
Ben Li-Sauerwine <bsauerwine@gmail.com>
2022-05-17 03:45:38 -0400

    Try not disabling normal networking?

--- custom_pigen/stage2/04-install-requirements/03-run.sh
+++ custom_pigen/stage2/04-install-requirements/03-run.sh
@@ -49,13 +49,13 @@ EOF
 # Following instructions at:
 # https://raspberrypi.stackexchange.com/questions/93311/switch-between-wifi-client-and-access-point-without-reboot
 
+# Is... this preventing setup from completing?
 # Disable Debian networking and dhcpcd
-# Skipped steps:
-#        systemctl mask networking.service dhcpcd.service
+# on_chroot << EOF
+#         systemctl mask networking.service dhcpcd.service
 #         mv /etc/network/interfaces /etc/network/interfaces-
-on_chroot << EOF
-        sed -i '1i resolvconf=NO' /etc/resolvconf.conf
-EOF
+#         sed -i '1i resolvconf=NO' /etc/resolvconf.conf
+# EOF
 
 # Enable systemd-networkd
 on_chroot << EOF
