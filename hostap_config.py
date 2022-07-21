61ef948f19a2a45680968c3c8f3eb3988284f469
Ben Li-Sauerwine <bal45@duke.edu>
2022-07-21 05:05:13 -0400

    Adds beginnings of HostAP configuration page.

--- hostap_config.py
+++ hostap_config.py
@@ -1,8 +1,18 @@
-from flask import Flask
+from flask import Flask, render_template
+import os
 
 app = Flask(__name__)
 
-
 @app.route('/')
-def hello():
-  return 'This will be the SimpleAQ HostAP configuration tool.'
+def main():
+  return render_template(
+      'index.html',
+      simpleaq_logo='static/simpleaq_logo.png',
+      influx_org=os.getenv('influx_org'),
+      influx_bucket=os.getenv('influx_bucket'),
+      influx_token=os.getenv('influx_token'),
+      influx_server=os.getenv('influx_server'),
+      simpleaq_interval=os.getenv('simpleaq_interval'),
+      simpleaq_hostapd_name=os.getenv('simpleaq_hostapd_name'),
+      hostap_retry_interval_sec=os.getenv('hostap_retry_interval_sec'),
+      max_backlog_writes=os.getenv('max_backlog_writes'))
