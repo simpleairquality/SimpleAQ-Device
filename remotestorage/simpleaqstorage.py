41da40248fab1a9bb76b85a1904da9ecfaaa041f
Ben Li-Sauerwine <bsauerwine@gmail.com>
2022-10-08 07:04:34 -0400

    "self", not "args".

--- remotestorage/simpleaqstorage.py
+++ remotestorage/simpleaqstorage.py
@@ -8,14 +8,12 @@ from . import RemoteStorage
 class SimpleAQStorage(RemoteStorage): 
   def __init__(self, endpoint=None, bucket=None, organization=None, token=None):
     super().__init__(endpoint=endpoint, bucket=bucket, organization=organization, token=token)
-    self.token = token
-    self.endpoint = endpoint
 
   def write(self, data_json):
     encoder = MultipartEncoder(
         {
-            'id': args.bucket,
-            'token': args.token,
+            'id': self.bucket,
+            'token': self.token,
             'file': ('file', json.dumps(data_json), 'application/ndjson')
         })
 
