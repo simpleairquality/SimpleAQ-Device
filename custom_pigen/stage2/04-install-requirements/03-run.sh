8bd7f35876203a13e9e87988a9ea779d0f4bf864
Ben Li-Sauerwine <bsauerwine@gmail.com>
2022-12-12 04:34:13 -0500

    Hacky workaround for detecting build environment as a Jetson.

--- custom_pigen/stage2/04-install-requirements/03-run.sh
+++ custom_pigen/stage2/04-install-requirements/03-run.sh
@@ -8,8 +8,12 @@ on_chroot << EOF
 EOF
 
 # Also install Waveshare.
+# This is an absolute hack.
+# It doetects the build environment as Jetson, not RPi, and we must remove that library after.
+# TODO:  Remove it:  https://github.com/yskoht/waveshare-epaper/issues/1
 on_chroot << EOF
         pip install git+https://github.com/waveshare/e-Paper#egg=waveshare-epd\&subdirectory=RaspberryPi_JetsonNano/python
+        pip uninstall Jetson.GPIO 
 EOF
 
 # Set up a system-scoped systemd service.
