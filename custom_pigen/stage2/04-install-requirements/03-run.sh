091e7ebdc99c3d482cf1f39f68f5b89791a55f25
Ben Li-Sauerwine <bsauerwine@gmail.com>
2024-07-26 03:08:36 -0400

    OK, forget pipx.  Let's just sudo pip install for global scope.  If this works, we can drop the pipx package entirely.

--- custom_pigen/stage2/04-install-requirements/03-run.sh
+++ custom_pigen/stage2/04-install-requirements/03-run.sh
@@ -7,14 +7,9 @@ on_chroot << EOF
         systemctl enable resize2fs_once
 EOF
 
-# Make sure pipx has the proper path.
-on_chroot << EOF
-        pipx ensurepath
-EOF
-
 # Install SimpleAQ requirements.
 on_chroot << EOF
-        cat /simpleaq/requirements.txt | xargs -I {} pipx install --include-deps {}
+        sudo pip install -r /simpleaq/requirements.txt
 EOF
 
 # Set up a system-scoped systemd service.
