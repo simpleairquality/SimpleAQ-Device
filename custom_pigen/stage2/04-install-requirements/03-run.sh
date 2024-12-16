c61bae73f8ec67fdea39392baa5814425100bfc5
Ben Li-Sauerwine <bsauerwine@gmail.com>
2024-12-16 02:52:56 -0500

    Remove networking stuff from the run script.  We'll work from here to see if we can get networking up.

--- custom_pigen/stage2/04-install-requirements/03-run.sh
+++ custom_pigen/stage2/04-install-requirements/03-run.sh
@@ -2,22 +2,6 @@
 
 cp -R /simpleaq "${ROOTFS_DIR}"
 
-# Enable resize2fs, which apparently isn't executing now?
-on_chroot << EOF
-        systemctl enable resize2fs_once
-EOF
-
-# Ensure that i2c and spi are enabled.
-# But still use our usual boot config, which has essential changes to i2c.
-on_chroot << EOF
-        cp /boot/firmware/config.txt /boot/firmware/temp
-	SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_i2c 0
-        SUDO_USER="${FIRST_USER_NAME}" raspi-config nonint do_spi 0
-        cp /boot/firmware/temp /boot/firmware/config.txt
-        rm /boot/firmware/temp
-EOF
-
-
 # Install SimpleAQ requirements.
 on_chroot << EOF
         pip install --break-system-packages -r /simpleaq/requirements.txt
@@ -65,57 +49,57 @@ EOF
 # https://raspberrypi.stackexchange.com/questions/93311/switch-between-wifi-client-and-access-point-without-reboot
 
 # Disable Debian networking and dhcpcd
-on_chroot << EOF
-        systemctl mask networking.service dhcpcd.service
-EOF
+# on_chroot << EOF
+#         systemctl mask networking.service dhcpcd.service
+# EOF
 
 # Ensure that systemd-resolved is configured properly.
-on_chroot << EOF
-    apt-get install -y systemd-resolved
-    rm /etc/resolv.conf 
-    ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
-EOF
+# on_chroot << EOF
+#    apt-get install -y systemd-resolved
+#    rm /etc/resolv.conf 
+#    ln -s /run/systemd/resolve/resolv.conf /etc/resolv.conf
+# EOF
 
 # Enable systemd-networkd
-on_chroot << EOF
-        systemctl disable NetworkManager.service
-        systemctl enable systemd-networkd.service
-        systemctl enable systemd-resolved.service
-        systemctl start systemd-networkd.service
-        systemctl start systemd-resolved.service
-        ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
-EOF
+# on_chroot << EOF
+#         systemctl disable NetworkManager.service
+#        systemctl enable systemd-networkd.service
+#        systemctl enable systemd-resolved.service
+#        systemctl start systemd-networkd.service
+#        systemctl start systemd-resolved.service
+#        ln -sf /run/systemd/resolve/resolv.conf /etc/resolv.conf
+# EOF
 
-cp files/08-wlan0.network "${ROOTFS_DIR}/etc/systemd/network"
+# cp files/08-wlan0.network "${ROOTFS_DIR}/etc/systemd/network"
 
 # The hostap is on ap0.
-cp files/12-ap0.network          "${ROOTFS_DIR}/etc/systemd/network"
-cp files/resolved.conf           "${ROOTFS_DIR}/etc/systemd/resolved.conf"
-cp files/dnsmasq.conf            "${ROOTFS_DIR}/etc/dnsmasq.conf"
+# cp files/12-ap0.network          "${ROOTFS_DIR}/etc/systemd/network"
+# cp files/resolved.conf           "${ROOTFS_DIR}/etc/systemd/resolved.conf"
+# cp files/dnsmasq.conf            "${ROOTFS_DIR}/etc/dnsmasq.conf"
 
 # Copy hostapd.conf in
-cp files/hostapd.conf "${ROOTFS_DIR}/etc/hostapd/hostapd.conf"
-cp files/hostapd "${ROOTFS_DIR}/etc/default/hostapd"
+# cp files/hostapd.conf "${ROOTFS_DIR}/etc/hostapd/hostapd.conf"
+# cp files/hostapd "${ROOTFS_DIR}/etc/default/hostapd"
 
 # The hostap no longer conflicts with the wlan0 service.
-on_chroot << EOF
-        systemctl enable hostapd 
-        systemctl start hostapd 
-EOF
+# on_chroot << EOF
+#         systemctl enable hostapd 
+#         systemctl start hostapd 
+# EOF
 
 # Choose a better HostAP name than just "SimpleAQ" if nothing else is provided. 
 # This file also has firewall safeguards.
 cp files/rc.local "${ROOTFS_DIR}/etc/rc.local"
 
 # Add AP setup endpoint to /etc/hosts
-on_chroot << EOF
-         echo "" >> /etc/hosts
-         echo "192.168.4.1             simpleaq.setup" >> /etc/hosts
-EOF
+# on_chroot << EOF
+#          echo "" >> /etc/hosts
+#          echo "192.168.4.1             simpleaq.setup" >> /etc/hosts
+# EOF
 
 # Don't let logs get too big.
 on_chroot << EOF
-         sed -i '/SystemMaxUse/c\SystemMaxUse=10M' /etc/systemd/journald.conf
+  sed -i '/SystemMaxUse/c\SystemMaxUse=100M' /etc/systemd/journald.conf
 EOF
 
 # Disable firewall safeguards for debug builds.
