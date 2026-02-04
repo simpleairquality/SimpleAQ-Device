7087e2b0d4e19a0fc48921c5ea0b56801d433b5b
Ben Li-Sauerwine <bsauerwine@gmail.com>
2026-02-04 16:45:55 -0500

    Actually, the old method worked fine.

--- custom_pigen/stage2/04-install-requirements/03-run.sh
+++ custom_pigen/stage2/04-install-requirements/03-run.sh
@@ -3,12 +3,12 @@
 cp -R /simpleaq "${ROOTFS_DIR}"
 
 # Install SimpleAQ requirements.
-# We use two separate methods to try to force the upgrade to break system packages if needed.
+# We force the upgrade to break system packages if needed.
 on_chroot << EOF
-    rm -f /usr/lib/python3.*/EXTERNALLY-MANAGED
+    export PIP_BREAK_SYSTEM_PACKAGES=1
 
-    python3 -m pip install --upgrade pip --break-system-packages
-    python3 -m pip install --break-system-packages -r /simpleaq/requirements.txt
+    python3 -m pip install --upgrade pip
+    python3 -m pip install -r /simpleaq/requirements.txt
 EOF
 
 # Set up a system-scoped systemd service.
